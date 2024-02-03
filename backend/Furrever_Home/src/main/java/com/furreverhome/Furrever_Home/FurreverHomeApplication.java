005086edc3c6b03234c1bd6054ff78f7728991ea
Jaydipsinh Ranjitsinh Padhiyar <jy761996@dal.ca>
2024-02-03 16:22:48 -0400

    Initial Setup Commit

---
+++ backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/FurreverHomeApplication.java
@@ -0,0 +1,13 @@
+package com.furreverhome.Furrever_Home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurreverHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurreverHomeApplication.class, args);
+	}
+
+}
