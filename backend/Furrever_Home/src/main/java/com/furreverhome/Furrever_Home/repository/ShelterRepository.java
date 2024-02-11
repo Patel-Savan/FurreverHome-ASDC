0d57bb0b4f41be7a2d45687cc66a65fe5307ed9c
kparmar <khyatiparmar@dal.ca>
2024-02-11 06:27:44 -0400

    A JPA repository for shelter

---
+++ backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/repository/ShelterRepository.java
@@ -0,0 +1,7 @@
+package com.furreverhome.Furrever_Home.repository;
+
+import com.furreverhome.Furrever_Home.entities.Shelter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShelterRepository extends JpaRepository<Shelter, Long> {
+}
