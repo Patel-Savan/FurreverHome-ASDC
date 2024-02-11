50170ae9a4de9fe6ed96e6e9941d69ffef3e4b6c
kparmar <khyatiparmar@dal.ca>
2024-02-11 06:32:48 -0400

    Converted Interface to a class.

--- backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/services/AuthenticationService.java
+++ backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/services/AuthenticationService.java
@@ -1,6 +1,38 @@
 package com.furreverhome.Furrever_Home.services;
 
 import com.furreverhome.Furrever_Home.dto.*;
+import com.furreverhome.Furrever_Home.entities.User;
+import com.furreverhome.Furrever_Home.enums.Role;
+import com.furreverhome.Furrever_Home.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtService jwtService;
+
+
+    @PostConstruct
+    public void createAdminAccount() {
+        User adminAccount = userRepository.findByRole(Role.ADMIN);
+
+        if(adminAccount == null) {
+            User user = new User();
 
 public interface AuthenticationService {
     PetAdopterDto signup(PetAdopterSignupRequest signUpRequest);
