f5020bbe3f01c87ad4b31b309fcc2e2e5d18d6d7
kparmar <khyatiparmar@dal.ca>
2024-02-11 06:35:47 -0400

    Introduced abstraction in the PetAdopterSignupRequest.

--- backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/dto/PetAdopterSignupRequest.java
+++ backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/dto/PetAdopterSignupRequest.java
@@ -1,8 +1,10 @@
 package com.furreverhome.Furrever_Home.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Data;
 
 @Data
+@JsonTypeName("petadopter")
 public class PetAdopterSignupRequest extends SignupRequest{
 
     private String firstName;
@@ -12,4 +14,14 @@ public class PetAdopterSignupRequest extends SignupRequest{
     private String phone_number;
 
     private String address;
+
+    public PetAdopterSignupRequest(String email, String password, String role, String firstName, String lastName,
+                                   String phone_number, String address) {
+        super(email, password, role);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone_number = phone_number;
+    }
+
 }
