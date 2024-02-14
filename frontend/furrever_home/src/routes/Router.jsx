581c4e579ad31a789a9c091e020dcc00bb192121
Savan Maheshkumar Patel <sv272995@dal.ca>
2024-02-14 17:55:31 -0400

    Component : Add Reset Password Page

--- frontend/furrever_home/src/routes/Router.jsx
+++ frontend/furrever_home/src/routes/Router.jsx
@@ -4,7 +4,9 @@ import Home from '../pages/Home'
 import Layout from '../layouts/Layout'
 import PetAdopterRegister from '../components/Register/PetAdopterRegister'
 import ShelterRegister from '../components/Register/ShelterRegister'
-
+import Login from '../pages/login'
+import ForgotPassword from '../pages/ForgotPassword'
+import ResetPassword from '../pages/ResetPassword'
 
 const Router = () => {
     return (
@@ -41,6 +43,22 @@ const Router = () => {
             Route for Login
             */}
             <Route path="/login" element={<Login />} />
+
+            {/* 
+            @Route
+            @Component Returns ResetPassword Component
+
+            Route for ResetPassword
+            */}
+            <Route path="/reset-password" element={<ResetPassword />} />
+
+            {/* 
+            @Route
+            @Component Returns ForgotPassword Component
+
+            Route for ForgotPassword
+            */}
+            <Route path="/forgot-password" element={<ForgotPassword />} />
 
         </Routes>
     )
