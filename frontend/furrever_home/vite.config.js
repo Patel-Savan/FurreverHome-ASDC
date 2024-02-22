d2f71306e6d96241ec2087165a8266c73753bc41
Jaydipsinh Ranjitsinh Padhiyar <jy761996@dal.ca>
2024-02-22 03:51:24 -0400

    Frontend CICD Try

--- frontend/furrever_home/vite.config.js
+++ frontend/furrever_home/vite.config.js
@@ -14,4 +14,14 @@ export default defineConfig({
   //   }  
   // } ,
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+   },
+   server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+   },
 })
