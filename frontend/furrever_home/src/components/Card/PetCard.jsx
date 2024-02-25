af9f02f75592f14b71d62689ab003fc877d9ff4a
Savan Maheshkumar Patel <sv272995@dal.ca>
2024-02-25 15:25:12 -0400

    component : add card component for pet and shelter

---
+++ frontend/furrever_home/src/components/Card/PetCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const PetCard = (
+    type,
+    breed,
+    age,
+    thumbnailSrc,
+    thumbnailAlt = "Thumbnail",
+    className,
+    shelter,
+    city,
+    contact
+) => {
+  return (
+    <div className={`rounded-lg p-6 shadow-sm ${className}`}>
+            <div className="overflow-hidden rounded-lg">
+                <img
+                    className="w-full cursor-pointer transition duration-200 ease-in-out transform hover:scale-110 rounded-full h-auto"
+                    src={thumbnailSrc}
+                    alt={thumbnailAlt}
+                />
+            </div>
+            <h5 className="pt-5 text-[14px] font-normal text-primary block">
+                {type}
+            </h5>
+            <h3 className="w-full cursor-pointer transition duration-200 ease-in-out transform hover:scale-110 rounded-lg h-auto">
+                {breed},{age}
+            </h3>
+            <p className="font-normal text-gray-500 cursor-pointer text-lg duration-300 transition hover:text-[#FA5252] mt-2">
+                Currently at {shelter}, {city} 
+            </p>
+            <p className="font-normal text-gray-500 cursor-pointer text-lg duration-300 transition hover:text-[#FA5252] mt-2">
+                Want to adopt me ? contact at {contact}
+            </p>
+    </div>
+  )
+}
+
+export default PetCard
