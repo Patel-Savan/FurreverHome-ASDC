2a331a56a7dd3292666970b5a6207dde0ad08700
Jaydipsinh Ranjitsinh Padhiyar <jy761996@dal.ca>
2024-02-25 21:26:29 -0400

    fix: Frontend Config Updated

--- frontend/furrever_home/vite.config.js
+++ frontend/furrever_home/vite.config.js
@@ -4,14 +4,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8080/api',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   }  
-  // } ,
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+   },
+   server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+   },
+
 })
