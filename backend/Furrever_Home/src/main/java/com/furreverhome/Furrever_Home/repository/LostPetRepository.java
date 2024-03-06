d142ed4416fbb5ca8cb03f968b1e58ed47a59a2b
Jay Jagdishbhai Patel <jy356444@dal.ca>
2024-03-06 19:51:46 -0400

    feat: add register for lostpet following TDD

---
+++ backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/repository/LostPetRepository.java
@@ -0,0 +1,8 @@
+package com.furreverhome.Furrever_Home.repository;
+
+import com.furreverhome.Furrever_Home.entities.LostPet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LostPetRepository extends JpaRepository<LostPet, Long> {
+
+}
--- backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/services/petadopterservices/PetAdopterService.java
+++ backend/Furrever_Home/src/main/java/com/furreverhome/Furrever_Home/services/petadopterservices/PetAdopterService.java
@@ -1,7 +1,9 @@
 package com.furreverhome.Furrever_Home.services.petadopterservices;
 
 import com.furreverhome.Furrever_Home.dto.PetAdopterDto;
+import com.furreverhome.Furrever_Home.dto.lostpet.RegisterLostPetDto;
 import com.furreverhome.Furrever_Home.dto.petadopter.*;
+import com.furreverhome.Furrever_Home.entities.LostPet;
 
 import java.util.List;
 
