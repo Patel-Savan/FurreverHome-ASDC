e036f806c9caa8efb38c7feb7921a41dc7ce01ec
kparmar <khyatiparmar@dal.ca>
2024-03-20 14:08:25 -0300

    ShelterControllerTest added

--- backend/Furrever_Home/src/test/java/com/furreverhome/Furrever_Home/controller/ShelterControllerTest.java
+++ backend/Furrever_Home/src/test/java/com/furreverhome/Furrever_Home/controller/ShelterControllerTest.java
@@ -2,6 +2,7 @@ package com.furreverhome.Furrever_Home.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.furreverhome.Furrever_Home.dto.GenericResponse;
+import com.furreverhome.Furrever_Home.dto.Pet.PetAdoptionRequestResponseDto;
 import com.furreverhome.Furrever_Home.dto.Pet.PetDto;
 import com.furreverhome.Furrever_Home.dto.Pet.PetVaccineDto;
 import com.furreverhome.Furrever_Home.dto.shelter.RegisterPetRequest;
@@ -12,14 +13,21 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,7 +50,7 @@ public class ShelterControllerTest {
     }
 
     @Test
-    void registerPetInShelter_SuccessfulRegistration_ReturnsPetDto() throws Exception {
+    void registerPetInShelterSuccessfulRegistration() throws Exception {
         RegisterPetRequest request = new RegisterPetRequest();
         request.setType("Dog");
         request.setBreed("Labrador");
@@ -62,7 +70,47 @@ public class ShelterControllerTest {
     }
 
     @Test
-    void addVaccine_SuccessfulAddition_ReturnsOk() throws Exception {
+    void editPetInShelter_Success() {
+        RegisterPetRequest registerPetRequest = new RegisterPetRequest();
+        registerPetRequest.setType("Dog");
+        registerPetRequest.setBreed("Labrador");
+        registerPetRequest.setColour("Black");
+        registerPetRequest.setGender("Male");
+        registerPetRequest.setBirthdate(new Date());
+        registerPetRequest.setPetImage("dog.jpg");
+        registerPetRequest.setPetMedicalHistory("Good health");
+        registerPetRequest.setShelter(123L);
+        Long petID = 1L;
+        PetDto updatedPetDto = new PetDto();
+        updatedPetDto.setType(registerPetRequest.getType());
+        updatedPetDto.setBreed(registerPetRequest.getBreed());
+        updatedPetDto.setColour(registerPetRequest.getColour());
+        updatedPetDto.setGender(registerPetRequest.getGender());
+        updatedPetDto.setBirthdate(registerPetRequest.getBirthdate());
+        updatedPetDto.setPetImage(registerPetRequest.getPetImage());
+        updatedPetDto.setPetMedicalHistory(registerPetRequest.getPetMedicalHistory());
+
+        when(shelterService.editPet(eq(petID), any(RegisterPetRequest.class))).thenReturn(updatedPetDto);
+        ResponseEntity<PetDto> responseEntity = shelterController.editPetInShelter(petID, registerPetRequest);
+
+        verify(shelterService, times(1)).editPet(eq(petID), any(RegisterPetRequest.class));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(updatedPetDto, responseEntity.getBody());
+    }
+
+    @Test
+    void deletePetInShelterDeletedSuccessfully() {
+        Long petID = 2L;
+        when(shelterService.deletePet(eq(petID))).thenReturn(new GenericResponse("Pet deleted."));
+        ResponseEntity<GenericResponse> responseEntity = shelterController.deletePetInShelter(petID);
+        verify(shelterService, times(1)).deletePet(eq(petID));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        assertEquals("Pet deleted.", responseEntity.getBody().getMessage());
+    }
+
+    @Test
+    void addVaccineSuccessfulAddition() throws Exception {
         long petId = 2L;
         PetVaccineDto petVaccineDto = new PetVaccineDto();
         petVaccineDto.setVaccineName("Rabies");
@@ -77,7 +125,71 @@ public class ShelterControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    void changeAdoptedStatusChangedSuccessfully() {
+        Long petId = 1L;
+        String status = "adopted";
+        when(shelterService.changeAdoptedStatus(eq(petId), eq(status))).thenReturn(true);
+        ResponseEntity<?> responseEntity = shelterController.changeAdoptedStatus(petId, status);
+        verify(shelterService, times(1)).changeAdoptedStatus(eq(petId), eq(status));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
 
+    @Test
+    void changeAdoptedStatus_Failure_ReturnsNotFound() {
+        Long petId = 1L;
+        String status = "adopted";
+        when(shelterService.changeAdoptedStatus(eq(petId), eq(status))).thenReturn(false);
+        ResponseEntity<?> responseEntity = shelterController.changeAdoptedStatus(petId, status);
+        verify(shelterService, times(1)).changeAdoptedStatus(eq(petId), eq(status));
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+    }
+    @Test
+    void getPetInfoRetrievedSuccessfully() {
+        Long petID = 1L;
+        PetDto petDto = new PetDto();
+
+        when(petService.getPetInfo(eq(petID))).thenReturn(petDto);
+        ResponseEntity<PetDto> responseEntity = shelterController.getPetInfo(petID);
+
+        verify(petService, times(1)).getPetInfo(eq(petID));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        assertEquals(petDto, responseEntity.getBody());
+    }
+
+    @Test
+    void getPetAdoptionRequestsRetrievedSuccessfully() {
+        Long petID = 1L;
+        PetAdoptionRequestResponseDto adoptionRequestResponseDto = new PetAdoptionRequestResponseDto();
+        when(shelterService.getPetAdoptionRequests(eq(petID))).thenReturn(adoptionRequestResponseDto);
+        ResponseEntity<PetAdoptionRequestResponseDto> responseEntity = shelterController.getPetAdoptionRequests(petID);
+
+        verify(shelterService, times(1)).getPetAdoptionRequests(eq(petID));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        assertEquals(adoptionRequestResponseDto, responseEntity.getBody());
+    }
+
+    @Test
+    void getPetInShelter_ReturnsListOfPets() {
+        Long shelterID = 1L;
+        List<PetDto> pets = new ArrayList<>();
+        PetDto pet1 = new PetDto();
+        pet1.setPetID(1L);
+        pet1.setType("Dog");
+        pets.add(pet1);
+        when(shelterService.getPetsForShelter(eq(shelterID))).thenReturn(pets);
+        ResponseEntity<List<PetDto>> responseEntity = shelterController.getPetInShelter(shelterID);
+
+        verify(shelterService, times(1)).getPetsForShelter(eq(shelterID));
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+        assertEquals(pets.size(), responseEntity.getBody().size());
+        PetDto returnedPet = responseEntity.getBody().get(0);
+        assertEquals(pet1.getPetID(), returnedPet.getPetID());
+        assertEquals(pet1.getType(), returnedPet.getType());
+    }
 
     /**
      * util function to covert object to json
