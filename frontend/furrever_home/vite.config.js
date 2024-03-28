f40a77f5568872ba327e152d2b88c9b40dec454a
jpadhiyar <jy761996@dal.ca>
2024-03-28 11:16:14 -0300

    fix: frontend port issue

--- frontend/furrever_home/vite.config.js
+++ frontend/furrever_home/vite.config.js
@@ -1,19 +1,27 @@
-import { defineConfig} from 'vite'
+import { defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 // const env = loadEnv(mode, process.cwd(), '');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // console.log(App ENV: >>> ${JSON.stringify(env.VITE_BACKEND_BASE_URL)});
+  // console.log(App ENV SERVER PORT: >>> ${JSON.stringify(env.SERVER_PORT)});
+  // console.log(App ENV APP PORT: >>> ${JSON.stringify(env.APP_PORT)});
+  
+  return {
+    plugins: [react()],
   preview: {
-    port: 3000,
+    port: env.FRONTEND_APP_PORT,
     // strictPort: true,
    },
    server: {
-    port: 5173,
+    port: env.SERVER_PORT,
     strictPort: true,
     host: true,
     // origin: "http://0.0.0.0:5173",
-   },
-
+   },
+  };
 })
